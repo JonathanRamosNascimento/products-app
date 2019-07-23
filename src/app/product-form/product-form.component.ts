9bb6af0b7b06370b3423447e23045749b83c0374
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-07-22 23:54:01 -0300

    creating product and department model

--- src/app/product-form/product-form.component.ts
+++ src/app/product-form/product-form.component.ts
@@ -1,3 +1,4 @@
+import { Department } from './../models/department.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductFormComponent implements OnInit {
 
+  name: string;
+  department: Department;
+  price: number;
+  description: string;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  save() {
+
+  }
+
+  clear() {
+    
+  }
 }
