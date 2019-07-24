6f617c19b9c09b4f3ce7a1f9050777051d73d7d4
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-07-24 00:56:32 -0300

    criating data table of products

--- src/app/products-table/products-table.component.ts
+++ src/app/products-table/products-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-products-table',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsTableComponent implements OnInit {
 
-  constructor() { }
+  products: Product[];
+
+  prodColumns: string[] = ['id', 'name', 'price', 'description', 'department'];
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.products = this.productService.getProducts();
   }
 
 }
